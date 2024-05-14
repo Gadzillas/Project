11f06cd9fd9c04e87ebe3ac9ab20b3ccb33b9c43
Kupyrev <vip.kupyrev@mail.ru>
2024-05-14 16:23:30 +0500

    Added error backpropagation and weight adjustments

--- Net.py
+++ Net.py
@@ -24,7 +24,14 @@ class NeuralNetwork:
 
         final_inputs = numpy.dot(self.who, hidden_outputs)
         final_outputs = self.activation_function(final_inputs)
-        return final_outputs
+
+        output_errors = targets - final_outputs
+
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        numpy.transpose(inputs))
         pass
 
     def query(self, inputs_list):
@@ -36,6 +43,7 @@ class NeuralNetwork:
 
         final_inputs = numpy.dot(self.who, hidden_outputs)
         final_outputs = self.activation_function(final_inputs)
+
         return final_outputs
 
 
